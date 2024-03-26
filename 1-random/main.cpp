af2a17fc65e9d1ce96f1034a2a9aec77b4cb4130
Daniele95 <daniele.gamba1@studenti.unimi.it>
2024-03-26 16:05:43 +0100

    funzione test

--- 1-random/main.cpp
+++ 1-random/main.cpp
@@ -11,39 +11,12 @@ _/    _/  _/_/_/  _/_/_/_/ email: Davide.Galli@unimi.it
 #include <iostream>
 #include <fstream>
 #include <string>
-#include "random.h"
+#include "../random/random.h"
 
 using namespace std;
  
 int main (int argc, char *argv[]){
 
-   Random rnd;
-   int seed[4];
-   int p1, p2;
-   ifstream Primes("Primes");
-   if (Primes.is_open()){
-      Primes >> p1 >> p2 ;
-   } else cerr << "PROBLEM: Unable to open Primes" << endl;
-   Primes.close();
-
-   ifstream input("seed.in");
-   string property;
-   if (input.is_open()){
-      while ( !input.eof() ){
-         input >> property;
-         if( property == "RANDOMSEED" ){
-            input >> seed[0] >> seed[1] >> seed[2] >> seed[3];
-            rnd.SetRandom(seed,p1,p2);
-         }
-      }
-      input.close();
-   } else cerr << "PROBLEM: Unable to open seed.in" << endl;
-
-   for(int i=0; i<20; i++){
-      cout << rnd.Rannyu() << endl;
-   }
-
-   rnd.SaveSeed();
    return 0;
 }
 

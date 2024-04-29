f1fec596cd1507b770c95ca53244fef8f921dde0
Daniele95 <daniele.gamba1@studenti.unimi.it>
2024-04-29 19:34:52 +0200

    random walk

--- 2-randomWalk/randomWalk.cpp
+++ 2-randomWalk/randomWalk.cpp
@@ -11,13 +11,13 @@
 
 using namespace std;
 
-const int M = 100000;
-const int N = 100;
-const int L = int(M/N);
+const int M_campionamenti = 100000;
+const int N_blocchi = 100;
+const int L_step = int(M_campionamenti/N_blocchi);
 
-float dati[N];
-float medie[N];
-float deviazioni[N];
+float dati[N_blocchi];
+float medie[N_blocchi];
+float deviazioni[N_blocchi];
 
 Random rnd;
 
@@ -58,10 +58,10 @@ float tiroCasuali() {
    int intersezioni = 0;
    
    
-   for(int i=0; i<N; i++)
+   for(int i=0; i<N_blocchi; i++)
    {
       
-      while (tiri<L)
+      while (tiri<L_step)
       {
          o = rnd.Rannyu()*d;
          // punto in un quadrato di raggio 2L
@@ -81,7 +81,7 @@ float tiroCasuali() {
       
       }
       
-      dati[i]  /= float(L);
+      dati[i]  /= float(L_step);
       
       if(i==0) medie[i] = dati[i];
       
@@ -175,21 +175,6 @@ void integraleMontecarlo(){
 
 //-------------RANDOM WALK
 
-
-   int NRW = 1e4; //numero di random walks
-   int NBlocks = 1e2; //numero di blocchi in cui vengono raggruppati i random walks
-   int NSteps = 1e2; //numero massimo di passi che si vogliono considerare per un RW
-
-   vec SumAv(NBlocks,arma::fill::zeros);
-   vec ErrAv(NBlocks,arma::fill::zeros);
-   vec Sum2Av(NBlocks,arma::fill::zeros);
-
-  //Media a blocchi e incertezze
-   mat Average(NSteps, NBlocks); 
-   //matrice 1e2x1e2. Le righe indicizzano il passo, 
-   //le colonne la media sui blocchi del RW
-   mat Average2(NSteps, NBlocks);
-
    
 
 //funzione integranda
@@ -230,7 +215,10 @@ void integraleMontecarlo2()
       r(i) = Integrand(x(i)); // U[0,1) uniform distribution
    }
 
-   //tengo salvati nel main solo i vettori costd::ntenenti i dati che poi verranno salvati su file. Gli altri creati e distrutti all'interno della funzione MeanAndErr	
+   //tengo salvati nel main solo i vettori 
+   //costd::ntenenti i dati che poi verranno 
+   //salvati su file. Gli altri creati e 
+   //distrutti all'interno della funzione MeanAndErr	
    vec sum_prog(N,arma::fill::zeros);
    vec err_prog(N,arma::fill::zeros);
 
@@ -240,87 +228,52 @@ void integraleMontecarlo2()
     for (int i = 0; i < N; ++i) 
        outfile211 << sum_prog(i) << "\t" << err_prog(i) << endl;
     outfile211.close();
-
-
+    
    sum_prog.fill(0.);
    err_prog.fill(0.);
-
+   
    vec s(M);
    for (int i = 0; i < M; i++) 
-        s(i) = Function( LinGen(x(i)) ); // U[0,1) uniform distribution
-
-   mat RWC(3, NRW, arma::fill::zeros);   //Versione continua
-   mat Dist_RadC(NSteps,  NRW, arma::fill::zeros); //Versione continua
-   //Calcolo le distanze al quadrato per ogni passo per ogni RW
-   for (int l = 0; l < NSteps ; l++) { //ciclo su tutti i passi dei RW
-      for (int i = 0; i < NRW; i++) { //ciclo su tutti i 1e4 RW
-         double theta = acos(1.-2.*rnd.Rannyu());
-         double phi = 2*M_PI*rnd.Rannyu();
-         RWC(0,i) += sin(theta)*cos(phi);
-         RWC(1,i) += sin(theta)*sin(phi);
-         RWC(2,i) += cos(theta);
-         Dist_RadC(l,i) = pow(RWC(0,i),2) + 
-         	pow(RWC(1,i),2) + pow(RWC(2,i),2) ;          
-      }
-   }
-
-
-   //Media a blocchi e incertezze
-   Sum2Av.fill(0.);
-   SumAv.fill(0.);
-   ErrAv.fill(0.);
-
-   for (int l = 0; l < NSteps; l++) {
-      for (int i = 0; i < NBlocks; i++) {
-         double sum1 = 0.0;
-         for (int j = 0; j < L; j++) {
-            int k = j + i * L;
-            //cout <<l<<" "<<k<<endl;
-            if(k<1e4)
-            sum1 += Dist_RadC(l,k);
-         }
-         //Average(l,i) = sum1 / L; //media sull'i-esimo blocco all'l-esimo passo
-         //Average2(l,i) = pow(Average(l,i), 2);
-      }
-   }
-
-/*
-   for (int l = 0; l < NSteps; l++) {
-      for (int i = 0; i <= NBlocks; i++) {
-         SumAv(l) += Average(l,i);
-         Sum2Av(l) += Average2(l,i);
-      }
-      SumAv(l)/=NBlocks;
-      Sum2Av(l)/=NBlocks;
-      ErrAv(l) = sqrt((Sum2Av(l) - SumAv(l)*SumAv(l))/NBlocks);
-    }                     
-
-    //Stampa
-    ofstream outfile222("risultati/outfile222.txt");
-    for (int l = 0; l < NSteps; ++l) 
-       outfile222 << sqrt( SumAv(l) ) << "\t" << ErrAv(l)/(2*sqrt(SumAv(l))) << endl;
-
-    outfile222.close();
-    mediaBlocchi(s, sum_prog, err_prog, N, L);  //media a blocchi e incertezze
+        s(i) = Function( LinGen(x(i)) ); 
+        // U[0,1) uniform distribution
+   
+    mediaBlocchi(s, sum_prog, err_prog, N, L); 
 
     ofstream outfile212("risultati/outfile212.txt");
     for (int i = 0; i < N; ++i) 
        outfile212 << sum_prog(i) << "\t" << err_prog(i) << endl;
     outfile212.close();
+
     
-    */
 } 
- 
+
+
+
 
 void randomWalkDiscreto()
 {
-
-
+   int NRW = 1e4; //numero di random walks
+   int NBlocks = 1e2; //numero di blocchi in cui vengono raggruppati i random walks
    int L = NRW/NBlocks; //numero di random walks per blocco
+   int NSteps = 1e2; //numero massimo di passi che si vogliono considerare per un RW
+   
+   vec SumAv(NBlocks,arma::fill::zeros);
+   vec ErrAv(NBlocks,arma::fill::zeros);
+   vec Sum2Av(NBlocks,arma::fill::zeros);
 
-   mat RW(3, NRW, arma::fill::zeros);   //Matrice 3x1e4 posta a zero. Le righe inidicizzano la coordinata (x,y,z), le colonne il RW a passi fissati
-   mat Dist_Rad(NSteps,  NRW, arma::fill::zeros); //matrice 1e2x1e4. Le righe indicizzano il passo, le colonne il RW
+  //Media a blocchi e incertezze
+   mat Average(NSteps ,NBlocks); 
+   //matrice 1e2x1e2. Le righe indicizzano il passo, 
+   //le colonne la media sui blocchi del RW
+   mat Average2(NSteps,NBlocks); 
 
+   mat RW(3, NRW, arma::fill::zeros);   
+   //Matrice 3x1e4 posta a zero. Le righe
+   // inidicizzano la coordinata (x,y,z), 
+   //le colonne il RW a passi fissati
+   mat Dist_Rad(NSteps,  NRW, arma::fill::zeros); 
+   //matrice 1e2x1e4. Le righe indicizzano 
+   //il passo, le colonne il RW
    //Calcolo le distanze al quadrato per ogni passo per ogni RW
    for (int l = 0; l < NSteps ; l++) { //ciclo su tutti i passi dei RW
       for (int i = 0; i < NRW; i++) { //ciclo su tutti i 1e4 RW
@@ -338,14 +291,16 @@ void randomWalkDiscreto()
             sum1 += Dist_Rad(l,k);
          }
          Average(l,i) = sum1 / L; //media sull'i-esimo blocco all'l-esimo passo
-         Average2(l,i) = pow(Average(l,i), 2);
+         Average2(l,i) = pow(Average(l,i),2);
       }
    }
     
    for (int l = 0; l < NSteps; l++) {
         for (int i = 0; i <= NBlocks; i++) {
+        
+            if(i<NBlocks){//////////////////////////////////////
             SumAv(l) += Average(l,i);
-            Sum2Av(l) += Average2(l,i);
+            Sum2Av(l) += Average2(l,i);}
         }
         SumAv(l)/=NBlocks;
         Sum2Av(l)/=NBlocks;
@@ -362,7 +317,21 @@ void randomWalkDiscreto()
  
 void randomWalkContinuo()
 {
+   int NRW = 1e4; //numero di random walks
+   int NBlocks = 1e2; //numero di blocchi in cui vengono raggruppati i random walks
    int L = NRW/NBlocks; //numero di random walks per blocco
+   int NSteps = 1e2; //numero massimo di passi che si vogliono considerare per un RW
+   
+   vec SumAv(NBlocks,arma::fill::zeros);
+   vec ErrAv(NBlocks,arma::fill::zeros);
+   vec Sum2Av(NBlocks,arma::fill::zeros);
+
+  //Media a blocchi e incertezze
+   mat Average(NSteps ,NBlocks); 
+   //matrice 1e2x1e2. Le righe indicizzano il passo, 
+   //le colonne la media sui blocchi del RW
+   mat Average2(NSteps,NBlocks); 
+
    mat RWC(3, NRW, arma::fill::zeros);   //Versione continua
    mat Dist_RadC(NSteps, NRW, arma::fill::zeros); //Versione continua
 
@@ -398,8 +367,9 @@ void randomWalkContinuo()
 
    for (int l = 0; l < NSteps; l++) {
       for (int i = 0; i <= NBlocks; i++) {
+            if(i<NBlocks){//////////////////////////////////////
          SumAv(l) += Average(l,i);
-         Sum2Av(l) += Average2(l,i);
+         Sum2Av(l) += Average2(l,i);}
       }
       SumAv(l)/=NBlocks;
       Sum2Av(l)/=NBlocks;
@@ -418,14 +388,17 @@ void randomWalkContinuo()
 int main (int argc, char *argv[]){
 
 
+   rnd.SetSeed();   
+   int seed=23; 
+   rnd.SetPrimesCouple(seed);
    cout << tiroCasuali() << endl;
    
-   write(medie, N, "risultati/rMedia.txt");
-   write(deviazioni, N, "risultati/rErrore.txt");
+   write(medie, N_blocchi, "risultati/rMedia.txt");
+   write(deviazioni, N_blocchi, "risultati/rErrore.txt");
    
    integraleMontecarlo2();
-  // randomWalkDiscreto();
-   //randomWalkContinuo();
+   randomWalkDiscreto();
+   randomWalkContinuo();
    
    
    return 0;
